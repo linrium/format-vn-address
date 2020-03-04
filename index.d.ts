7e4bd7e2267d0961c1d413f36d13713ab7022084
linh <linrium@gmail.com>
2020-03-04 09:57:24 +0700

    Fixed typing

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,17 @@ declare module '@ahamove/format-vn-address' {
   export function format(str: string): string
   export function isVenue(str: string): string
   export function isAddress(str: string): string
-  export function extract(str: string): string
+
+  export type ExtractResult = Partial<{
+    country: string
+    region: string
+    county: string
+    locality: string
+    number: string
+    street: string
+    venue: string
+    address: string
+  }>
+
+  export function extract(str: string): ExtractResult
 }
